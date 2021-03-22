9c325f3ace94b0b8e53dbc45d66f8988716015ee
stphncrt <cretestephen@gmail.com>
2021-03-23 01:23:57 +0300

    database activated

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,31 +1,51 @@
 import React, {useState} from 'react';
 import {View, Text, FlatList} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+import moment from 'moment';
+
 import {timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 
+const user = auth().currentUser;
 const Timeline = () => {
-  // const user = auth().currentUser;
   // console.log(user);
 
   const [topicModalFlag, setTopicModalFlag] = useState(false);
   const [selectedTopic, setSelectedTopic] = useState(null);
+  const selectingTopic = value => {
+    setSelectedTopic(value);
+    setTopicModalFlag(false);
+
+    //     .on('value', snapshot => {
+    //       console.log('update');
+    //       console.log(snapshot.val());
+    //     });
+  };
+  const sendingPost = value => {
+    const postObject = {
+      userMail: user.email,
+      postText: value,
+      time: moment().toISOString(),
+    };
+    database().ref(`${selectedTopic}/`).push(postObject);
+  };
   return (
     <View style={timelinePage.container}>
       <View style={timelinePage.container}>
-        <Header onTopicModalSelect={() => setTopicModalFlag(true)} />
+        <Header
+          title={selectedTopic}
+          onTopicModalSelect={() => setTopicModalFlag(true)}
+        />
 
         <FlatList data={[]} renderItem={() => null} />
 
-        <PostInput onSendingPost={x => console.log(x)} />
+        <PostInput onSendingPost={sendingPost} />
 
         <TopicSelectModal
           visibility={topicModalFlag}
           onClose={() => setTopicModalFlag(false)}
-          onTopicSelect={value => {
-            setSelectedTopic(value);
-            setTopicModalFlag(false);
-          }}
+          onTopicSelect={selectingTopic}
         />
       </View>
     </View>
